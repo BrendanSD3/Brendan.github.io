e53fd66ed7beebd1679930fecb40f876370143f1
Brendan Wall <BWall@enterprise-ireland.com>
2020-10-29 11:47:18 +0000

    Some changes to angular

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,6 +1,7 @@
 
-var testApp = angular.module("myApp", []);
+/* var testApp = angular.module("myApp", []);
 
 myApp.controller("myController",function($scope,$http){
     $scope.title="My_Cv";
 });
+ */
