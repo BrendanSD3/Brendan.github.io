7af0083cd93d161deff7a06314a91cb07791a55c
Brendan Wall <BWall@enterprise-ireland.com>
2020-11-16 18:05:31 +0000

    link to script file

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -5,3 +5,10 @@ myApp.controller("myController",function($scope,$http){
     $scope.title="My_Cv";
 });
  */
+  
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    $('.cards').removeClass('.w3-white').addClass('.w3-black');
+
+}
